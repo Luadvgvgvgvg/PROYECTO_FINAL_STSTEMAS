6789a0c3f9866d762113718300e6885f2ff8ab82
Luadvgvgvgvg <n00475858@upn.pe>
2025-11-13 15:15:14 -0500

    Add files via upload

---
+++ BibliotecaAlertas/BibliotecaAlertas/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaAlertas
+{
+    public class SistemaAlertas
+    {
+        public void Evaluar(int temperatura, int humo)
+        {
+            if (temperatura > 70 && humo > 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ALERTA CRÍTICA: RIESGO ALTO DE INCENDIO");
+                AlarmaCritica();
+            }
+            else if (temperatura > 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("ADVERTENCIA: Temperatura alta detectada.");
+                AlarmaAdvertencia();
+            }
+            else if (humo > 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("ADVERTENCIA: Nivel de humo elevado.");
+                AlarmaAdvertencia();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Condiciones normales. Sistema estable.");
+            }
+
+            Console.ResetColor();
+        }
+
+        private void AlarmaCritica()
+        {
+            // Sonido fuerte y repetitivo
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Beep(900, 300);
+                Console.Beep(700, 300);
+            }
+        }
+
+        private void AlarmaAdvertencia()
+        {
+            // Sonido más suave
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Beep(500, 200);
+            }
+        }
+    }
+}
+
---
+++ BibliotecaMonitoreo/BibliotecaMonitoreo/Class1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BibliotecaMonitoreo
+{
+    public class SistemaMonitoreo
+    {
+        Random rnd = new Random();
+
+        public int ObtenerTemperatura()
+        {
+            int temperatura = rnd.Next(15, 81);
+            return temperatura;
+        }
+
+        public double ObtenerHumo(int temperatura)
+        {
+            double humo = (temperatura * 1.2) + rnd.Next(-10, 11);
+
+            if (humo < 0)
+            {
+                humo = 0;
+            }
+
+            if (humo > 100)
+            {
+                humo = 100;
+            }
+
+            return Math.Round(humo, 1);
+        }
+
+        public string Diagnosticar(int temperatura, double humo)
+        {
+            if (temperatura < 40 && humo < 30)
+            {
+                return "Condiciones normales.";
+            }
+            else if ((temperatura >= 40 && temperatura < 60) || (humo >= 30 && humo < 60))
+            {
+                return "Advertencia: temperatura o humo elevados.";
+            }
+            else
+            {
+                return "PELIGRO: Riesgo de incendio detectado.";
+            }
+        }
+
+        public void MostrarDatos(string zona)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("MONITOREANDO " + zona + "...");
+            Console.ResetColor();
+            Console.WriteLine("----------------------------------------------");
+
+            int temperatura = ObtenerTemperatura();
+            double humo = ObtenerHumo(temperatura);
+            string diagnostico = Diagnosticar(temperatura, humo);
+
+            Console.Write("Temperatura: ");
+            if (temperatura >= 60)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (temperatura >= 40)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            Console.WriteLine(temperatura + " °C");
+            Console.ResetColor();
+
+            Console.Write("Nivel de humo: ");
+            if (humo >= 60)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (humo >= 30)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            Console.WriteLine(humo + "%");
+            Console.ResetColor();
+
+            Console.WriteLine("----------------------------------------------");
+
+            if (diagnostico.Contains("PELIGRO"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(diagnostico);
+                Console.ResetColor();
+                AlertaSonoraCritica();
+            }
+            else if (diagnostico.Contains("Advertencia"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(diagnostico);
+                Console.ResetColor();
+                AlertaSonoraAdvertencia();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(diagnostico);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Presione una tecla para volver al menú...");
+            Console.ReadKey();
+        }
+
+        public void AlertaSonoraAdvertencia()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Beep(700, 250);
+                Thread.Sleep(120);
+            }
+        }
+
+        public void AlertaSonoraCritica()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Beep(1000, 300);
+                Thread.Sleep(150);
+                Console.Beep(800, 300);
+                Thread.Sleep(150);
+            }
+        }
+    }
+}
